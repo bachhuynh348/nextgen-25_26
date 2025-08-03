f6424f42a5c05616cda48fa13a9eff26acf0ffee
ALEX <bach82486@gmail.com>
2025-08-03 14:56:16 +0700

    Add files via upload

---
+++ NEXTGEN/bt1.py
@@ -0,0 +1,6 @@
+from PyQt6 import QtWidgets,QtGui,QtCore
+from PyQt6.QtWidgets import QMessageBox,QWidget,QMainWindow,QWidgetItem,QApplication
+from PyQt6 import uic
+import re
+import sys
+import json
